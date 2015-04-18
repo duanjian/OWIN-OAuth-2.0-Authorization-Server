eed7b11d71e6b1bfe9a5a44b57988f2df3bc1f57
duanjian <iduanjian@126.com>
2015-04-18 10:23:12 +0800

    first commit
    a owin oauth2 authorization server

---
+++ C#/ResourceServer/Controllers/MeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ResourceServer.Controllers
+{
+    [Authorize]
+    public class MeController : ApiController
+    {
+        public string Get()
+        {
+            return this.User.Identity.Name;
+        }
+    }
+}
